078d7e3f98fa709d9098d03972e7a2542bf31457
ellaxu43 <ella.xu43@gmail.com>
2022-04-06 23:47:17 -0500

    add files

---
+++ dataprep.py
@@ -0,0 +1,151 @@
+
+import os
+import pandas as pd
+import env
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import env
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+
+
+from env import user, password, host
+
+
+database_url_base = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/'
+
+
+def get_zillow_data(use_cache=True):
+    if os.path.exists('zillow.csv') and use_cache:
+        print('Using cached csv')
+        df = pd.read_csv('zillow.csv')
+       
+        return df
+    
+    
+    print('Acquiring data from SQL database')
+    query = """SELECT prop.*, 
+       pred.logerror, 
+       air.airconditioningdesc, 
+       arch.architecturalstyledesc, 
+       build.buildingclassdesc, 
+       heat.heatingorsystemdesc, 
+       landuse.propertylandusedesc, 
+       story.storydesc, 
+       construct.typeconstructiondesc 
+       FROM   properties_2017 prop  
+       INNER JOIN (SELECT parcelid,logerror,
+                          Max(transactiondate) transactiondate 
+                   FROM   predictions_2017 
+                   GROUP  BY parcelid, logerror) pred
+               USING (parcelid) 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
+       LEFT JOIN storytype story USING (storytypeid) 
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
+        WHERE  prop.latitude IS NOT NULL 
+       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31' 
+"""
+    df = pd.read_sql(query, database_url_base + 'zillow')
+    df.to_csv('zillow.csv', index=False)
+   
+    return df
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+def remove_columns(df, cols_to_remove):  
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+
+def wrangle_zillow():
+    df = pd.read_csv('zillow.csv')
+    
+    # Restrict df to only properties that meet single unit use criteria
+    single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
+    df = df[df.propertylandusetypeid.isin(single_use)]
+    
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
+
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+    
+       
+    # drop columns not needed
+    df = remove_columns(df, ['id',
+       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
+       ,'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 
+        'censustractandblock', 'propertylandusedesc','heatingorsystemdesc','unitcnt'
+                            ,'buildingqualitytypeid','transactiondate','regionidcity',
+                             'parcelid'])
+
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+#     df.buildingqualitytypeid.fillna(6.0, inplace = True)
+
+    # Columns to look for outliers
+    df = df[df.taxvaluedollarcnt < 5_000_000]
+    df[df.calculatedfinishedsquarefeet < 8000]
+    
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
+    
+    #rename the columns
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built'})
+    
+    return df
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+        
+    return df
+    
+    
+def removed_outliers(df):
+    
+    df = remove_outliers(df, 1.5, [ 'bathrooms','bedrooms','area'])
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
